f61807605f0f43670c05439adf6089ac03a5420a
Vishwa Wijekoon <vishwamadusanka1022@gmail.com>
2025-09-18 15:54:21 +0530

    schema

--- src/modules/meetings/schemas.ts
+++ src/modules/meetings/schemas.ts
@@ -9,13 +9,3 @@ export const meetingsUpdateSchema = meetingsInsertSchema.extend({
     id: z.string().min(1, {message: "ID is required"}),
 });
 
-// ++ ADD a schema for updating the summary
-export const meetingsUpdateSummarySchema = z.object({
-    id: z.string().min(1, { message: "ID is required" }),
-    summary: z.string(), // Allow empty string for creating a blank summary
-});
-
-// ++ ADD a schema for deleting the summary
-export const meetingsDeleteSummarySchema = z.object({
-    id: z.string().min(1, { message: "ID is required" }),
-});
